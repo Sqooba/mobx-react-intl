101936dac7c523acb5a2767bf2cb94341bd92f25
Christopher Chiche <christopher.chiche@gmail.com>
2017-10-02 17:36:38 +0200

    💚Fix typo

--- src/MobxIntlProvider.tsx
+++ src/MobxIntlProvider.tsx
@@ -3,7 +3,7 @@ import { LocaleStore } from "./LocaleStore";
 import {inject, observer} from "mobx-react";
 import {IntlProvider} from "react-intl"
 
-const MobxIntlProviderChild =  ({locale, children, defaultLocale}: {locale: LocaleStore, children: any, defaultLocale?:String}) => {
+const MobxIntlProviderChild =  ({locale, children, defaultLocale}: {locale: LocaleStore, children: any, defaultLocale?:string}) => {
     const loc = locale.value;
     const messages = locale.messages;
     return <IntlProvider key={loc} defaultLocale={defaultLocale} locale={loc} messages={messages}>
